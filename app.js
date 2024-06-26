4008465ef91e7b6a0af1747cffedcf33763b7d00
Aniya Stanford <91031207+stanfordaniya@users.noreply.github.com>
2024-06-26 11:01:07 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); // send HTML file on GET request
+});
+
+app.post('/submit-form', (req, res) => {
+    const username = req.body.username; // access form data
+    // Add validation logic here
+    res.send(`Username is $astanf12`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
